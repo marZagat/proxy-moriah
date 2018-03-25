1ccac6939dced867d6a7bea017de7281f2c579ab
Moriah Rahamim <moriahfaye@gmail.com>
2018-03-25 14:31:58 -0700

    clean up legacy proxy and make it work

--- server.js
+++ server.js
@@ -1,30 +1,33 @@
-const express = require('express')
+const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 const request = require('request');
 
+// TODO: remove hardcoded values
+const ADDRESSES = {
+  'gallery': 'localhost:3001',
+  'overview': 'localhost:3002',
+  'sidebar': 'localhost:3003',
+  'recommendations': 'localhost:3004',
+};
+
 app.use(morgan('dev'));
-// app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.redirect('/restaurants/ChIJUcXYWWGAhYARmjMY2bJAG2s/');
+  const randomId = Math.floor(Math.random() * 10000000);
+  res.redirect(`/restaurants/${randomId}`);
 });
 
 app.use('/restaurants/:id', express.static(path.join(__dirname, 'public')));
 
-app.get('/api/restaurants/:id/gallery', (req, res) => {
-  res.redirect(`http://13.57.148.57/api/restaurants/${req.params.id}/gallery`)
-});
-app.get('/api/restaurants/:id/overview', (req, res) => {
-  res.redirect(`http://184.169.248.150/api/restaurants/${req.params.id}/overview`)
-});
-app.get('/api/restaurants/:id/sidebar', (req, res) => {
-  res.redirect(`http://54.177.233.239/api/restaurants/${req.params.id}/sidebar`)
-});
-app.get('/api/restaurants/:id/recommendations', (req, res) => {
-  res.redirect(`http://52.89.102.101/api/restaurants/${req.params.id}/recommendations`)
+app.get('/api/restaurants/:id/:service', ({ params: { id, service } }, res) => {
+  if (!ADDRESSES[service]) {
+    return res.status(400).send();
+  }
+
+  res.redirect(`http://${ ADDRESSES[service] }/api/restaurants/${ id }/${ service }`)
 });
 
 app.listen(port, () => {
