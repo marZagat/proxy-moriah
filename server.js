6a0fbd615abd9bfcff9c424e48c0a374957b7349
Moriah Rahamim <moriahfaye@gmail.com>
2018-03-25 17:34:22 -0700

    clean up server logic and file references

--- server.js
+++ server.js
@@ -1,35 +1,22 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const services = require('./service-config');
+
 const app = express();
-const port = process.env.PORT || 3000;
-const request = require('request');
-
-// TODO: remove hardcoded values
-const ADDRESSES = {
-  'gallery': 'localhost:3001',
-  'overview': 'localhost:3002',
-  'sidebar': 'localhost:3003',
-  'recommendations': 'localhost:3004',
-};
-
 app.use(morgan('dev'));
+app.use('/restaurants/:id', express.static(path.resolve(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   const randomId = Math.floor(Math.random() * 10000000);
   res.redirect(`/restaurants/${randomId}`);
 });
 
-app.use('/restaurants/:id', express.static(path.join(__dirname, 'public')));
-
-app.get('/api/restaurants/:id/:service', ({ params: { id, service } }, res) => {
-  if (!ADDRESSES[service]) {
-    return res.status(400).send();
-  }
-
-  res.redirect(`http://${ ADDRESSES[service] }/api/restaurants/${ id }/${ service }`)
+app.get('/api/restaurants/:id/:serviceName', ({ params: { id, serviceName } }, res) => {
+  if (!services[serviceName]) return res.status(400).send();
+  res.redirect(`http://${ ADDRESSES[service] }/api/restaurants/${ id }/${ serviceName }`)
 });
 
 app.listen(port, () => {
-  console.log(`server running at: http://localhost:${port}`)
+  console.log(`server running at: http://localhost:3000`)
 });
