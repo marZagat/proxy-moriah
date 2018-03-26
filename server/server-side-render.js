6531794800f58fc46856b6d4c72252d44713356d
Moriah Rahamim <moriahfaye@gmail.com>
2018-03-26 15:26:00 -0700

    return complete html and use obj for componentStrings

--- server/server-side-render.js
+++ server/server-side-render.js
@@ -2,15 +2,16 @@ const React = require('react');
 const ReactDom = require('react-dom/server');
 const path = require('path');
 
-// const Layout = require('./templates/layout');
-// const App = require('./templates/app');
-// const Scripts = require('./templates/scripts');
+const Layout = require('../templates/layout');
+const App = require('../templates/app');
+// const Scripts = require('../templates/scripts');
 const SERVICES = require('./microservices/service-config');
 const fetchBundleFiles = require('./microservices/service-loader');
 
 // get bundle files for each service, load react components to services object
 const clientBundlesFolder = path.resolve(__dirname, '../public/services');
 const serverBundlesFolder = path.resolve(__dirname, '../templates/services');
+fetchBundleFiles(clientBundlesFolder, serverBundlesFolder, SERVICES);
 
 const renderComponentToString = (reactComponent, props = {}) => {
   return ReactDom.renderToString(
@@ -19,21 +20,22 @@ const renderComponentToString = (reactComponent, props = {}) => {
 };
 
 const getComponentStrings = (services, props = {}) => {
-  const serviceNames = Object.keys(services);
-  return serviceNames.map((serviceName) => {
+  const componentStrings = {};
+  for (let serviceName in services) {
     const { reactComponent } = services[serviceName];
-    return renderComponentToString(reactComponent, props);
-  });
+    componentStrings[serviceName] = renderComponentToString(reactComponent, props);
+  }
+  return componentStrings;
 };
 
 const createStaticPage = (services, pageTitle, id) => {
   const componentStrings = getComponentStrings(services, { id });
 
-  // return Layout(
-  //   pageTitle,
-  //   App(...componentStringsArr)
-  // );
+  return Layout(
+    pageTitle,
+    App(componentStrings),
+    // Scripts( someInputs ),
+  );
 };
 
 module.exports = createStaticPage;
-
